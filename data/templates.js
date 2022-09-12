3b1156bfca3f9591daaf0c700bd0af63ce004e82
Arjun Ladha <tigerninja2234@gmail.com>
2022-09-12 16:56:58 +0530

    feat: create .gitignore template and creation system

--- data/templates.js
+++ data/templates.js
@@ -2,3 +2,8 @@ export async function READMEmdTemplate(name, version, description, author) {
     // Function to return the README.md template
     return `# ${name} | \`${version}\`\n\n## ${description}\n\n### By: ${author}`
 }
+
+export async function gitignoreTemplate() {
+    // Function to return the .gitignore template
+    return `node_modules`
+}
--- utils/createFiles.js
+++ utils/createFiles.js
@@ -1,10 +1,12 @@
-import { appendFile } from "fs";
+import { appendFile, createFile } from "fs";
 import { createSpinner } from "nanospinner";
-import { READMEmdTemplate } from "../data/templates.js";
+import { READMEmdTemplate, gitignoreTemplate } from "../data/templates.js";
 
 
 export async function createFiles(info) {
     // create the files
+    
+    // creating README.md
     const ReadmeMD_Spinner = createSpinner('Creating README.md').start();
     const READMEmdContent = READMEmdTemplate(info[0], info[1], info[2], info[3]);
     appendFile('README.md', await READMEmdContent, function (err) {
@@ -13,5 +15,15 @@ export async function createFiles(info) {
             throw err
         };
         ReadmeMD_Spinner.success({ text: 'Created README.md', color: 'green' });
+    });
+
+    // Creating .gitignore
+    const gitignore_Spinner = createSpinner('Creating .gitignore').start();
+    const gitignoreContent = gitignoreTemplate();
+    appendFile('.gitignore', await gitignoreContent, function (err) {
+        if ( err ) {
+            gitignore_Spinner.error({ text: 'Couldn\'t create .gitignore', color: 'red' });
+        }
+        gitignore_Spinner.success({ text: 'Created .gitignore', color: 'green' });
     });
 };
